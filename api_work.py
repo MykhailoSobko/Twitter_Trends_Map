eaf3b824174b12ca6ecc36ba582e0834c7a101ca
agravitybrain <tsymbalistayaroslav.2002@gmail.com>
2021-05-17 18:17:39 +0300

    test for api_work

--- api_work.py
+++ api_work.py
@@ -8,28 +8,31 @@ bearer_token = "BEARER TOKEN REMOVED"
 def get_place_woeid(country=None, city=None, path='place_woeids.json'):
     '''Get woeid parameter for forming request to trends API.
     If country is specified, return its woeid, if country and city are None,
-    return Worldwide woeid which equals to 1'''
-    if not country and not city:
-        return 1
-
+    return Worldwide woeid which equals to 1'''    
     # look for woeids in a ready file
     with open(path, 'r', encoding='utf-8') as read_file:
         data = json.load(read_file)
+    
+    if not country:
+        woeid = 1
+    else:
+         woeid = None
 
     countries = {item['name']:item['woeid'] for item in data if item['placeType']['code']==12}
-    if country:
-        if country in countries:
-            woeid = countries[country]
-            return woeid
-        else:
-            return None
     
+    if type(country)==str:
+        if country in countries:
+            woeid = countries[country]    
+
     # if city is given
-    if city:
+    if city and woeid:
         cities = {item['name']:item['woeid'] for item in data if item['placeType']['code']==7 and item['parentid']==woeid}
-        if city in cities:
+        if type(city)==str and city in cities:
             woeid = cities[city]
-            return woeid
+        elif city:
+            return None
+
+    return woeid
 
 
 def get_json(country=None, city=None):
@@ -49,8 +52,3 @@ def get_json(country=None, city=None):
     }
     response = requests.get(base_url, headers=search_headers)
     return response.json()[0]
-
-
-if __name__ == '__main__':
-    trends = get_json('United Kingdom')
-    pprint(trends)
